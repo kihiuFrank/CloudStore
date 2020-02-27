aca23dafdab66f74bbccce325cc25b2fd6f7bab1
kihiuFrank <franklinekihiu@gmail.com>
2020-02-27 14:31:08 +0300

    UI Fix

---
+++ app/src/main/java/com/example/cloudstore/SecurityUtil.java
@@ -0,0 +1,46 @@
+package com.example.cloudstore;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class SecurityUtil {
+    private static String algorithm = "AES";
+    public static SecretKey generateKey() throws NoSuchAlgorithmException {
+        // Generate a 256-bit key
+        final int outputKeyLength = 256;
+        SecureRandom secureRandom = new SecureRandom();
+        // Do *not* seed secureRandom! Automatically seeded from system entropy.
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(outputKeyLength, secureRandom);
+        SecretKey yourKey = keyGenerator.generateKey();
+        return yourKey;
+    }
+    public static byte[] encodeFile(SecretKey yourKey, byte[] fileData)
+            throws Exception {
+        byte[] encrypted = null;
+        byte[] data = yourKey.getEncoded();
+        SecretKeySpec skeySpec = new SecretKeySpec(data, 0, data.length,
+                algorithm);
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec, new IvParameterSpec(
+                new byte[cipher.getBlockSize()]));
+        encrypted = cipher.doFinal(fileData);
+        return encrypted;
+    }
+    public static byte[] decodeFile(SecretKey yourKey, byte[] fileData)
+            throws Exception {
+        byte[] decrypted = null;
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.DECRYPT_MODE, yourKey, new IvParameterSpec(
+                new byte[cipher.getBlockSize()]));
+        decrypted = cipher.doFinal(fileData);
+        return decrypted;
+    }
+
+}
