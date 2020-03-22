45952ae0f339d75f3c0ef150cbbf13fde38a55e6
kihiuFrank <franklinekihiu@gmail.com>
2020-03-22 23:39:17 +0300

    splash

--- app/src/main/java/com/example/cloudstore/SplashScreen.java
+++ app/src/main/java/com/example/cloudstore/SplashScreen.java
@@ -9,7 +9,7 @@ import android.os.Handler;
 public class SplashScreen extends AppCompatActivity {
 
     /** Duration of wait **/
-    private final int SPLASH_DISPLAY_LENGTH = 4000;
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
