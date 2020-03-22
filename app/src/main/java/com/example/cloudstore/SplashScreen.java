f6cb8469593241ede2f0d4707ed938b98bd63b25
kihiuFrank <franklinekihiu@gmail.com>
2020-03-22 23:38:14 +0300

    splash

---
+++ app/src/main/java/com/example/cloudstore/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.cloudstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this,ListActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+}
